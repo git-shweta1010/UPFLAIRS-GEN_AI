b532eee31a7304bd1eed284e4246d48c948e775e
jaikrishann <jaikrishan2001@gmail.com>
2025-06-25 18:12:02 +0530

    new

---
+++ python/str.py
@@ -0,0 +1,65 @@
+# print(name.upper())   ##uppercase string
+# print(name.lower())   ##lowercase string
+# print(name.title())    #Data Sci 
+# print(name.capitalize())  #Upflairs
+# print(name.count("a"))
+# print(name.find("pvt"))
+# print(name.index("pvt"))
+# print(name.replace("pvt","pvt ltd"))
+# print(name.split(" "))
+# print(name.strip())  
+# print(name.lstrip())
+# print(name.rstrip())
+
+
+
+# print(name.isalnum())   ##alphanumeric
+# print(name.isalpha())   ##alphabetic
+# print(name.isdigit())   ##numeric
+# print(name.islower()) 
+# print(name.isupper())
+# print(name.isspace())
+# print(name.endswith("pvt"))
+# print(name.startswith("Upflai"))
+# print(name.casefold())
+# print(name.center(50,"h"))
+# print(name.ljust(50,"*"))
+# print(name.rjust(50,"*"))
+# print(name.zfill(50))
+# print(name.swapcase())
+# print(name.encode())
+name = "UpflaiRs pvt"
+# print(name.partition("z"))
+# print(name.rpartition("pvt"))
+# print(name.rfind("pvt"))
+# print(name.rindex("pvt"))
+# print(name.rsplit("pvt"))
+# print(name.removesuffix("pvt"))
+# print(name.removeprefix("UpflaiRs"))
+
+
+
+
+
+
+
+
+
+
+
+
+#  h e l l o 
+#  0 1 2 3 4 -- positive
+# -5 -4 -3 -2 -1 -- negative
+#positive  -- 0 to no of characters
+#negative -- -1 to -no of characters
+##indexing -- accessing particular/single  character
+#slicing -- accessing multiple characters
+# name = "UpflaiRs pvt"
+# print(name[-8])
+##slicing -- [start:end:step/skip] --- n to n-1
+# print(name[::-1])
+##formatted string //f - string 
+# age = 10 
+# text = f"my age is {age}"
+# print(text)
